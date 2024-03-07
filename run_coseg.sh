ce984e88137f97ec780c2a5a845909463347030b
EthanZhangYC <850704720@qq.com>
2024-03-07 14:01:48 +0700

    update

--- run_coseg.sh
+++ run_coseg.sh
@@ -132,13 +132,9 @@ python main_simclr.py \
 
 
 
-
-
-
-
-CUDA_VISIBLE_DEVICES=0,1 \
+CUDA_VISIBLE_DEVICES=3 \
 python main_cedice_ours.py  \
---exp_dir results/0123_bs8_DiceCE_normaltransform_nolradjust_coseglocalfinalpretrain_1 \
+--exp_dir results/0306_bs8_DiceCE_normaltransform_nolradjust_cdsfinalpretrain \
 --batch_size 8 \
 --epochs 150 \
 --mode stride \
@@ -146,10 +142,39 @@ python main_cedice_ours.py  \
 --save_freq 50 \
 --print_freq 100 \
 --temp 0.1 \
---resume /home/ziyuan/yichen/semi_cotrast_seg/results/0119_bs1_stride_Con_1gpu_lr000001_wd1e4_simclrfinalpretrain_nonormalize/models/SupCon_mmwhs_adam_fold_1_lr_1e-05_decay_0.0001_bsz_1_temp_0.1_train_1.0_stride_stride_4/ckpt.pth \
---gpu 2,3
+--resume /home1/yichen/SemiContrast/results/0119_fakemr_CDS_sgd_lr3e3_wd5e4_momentum09/checkpoints/checkpoint.pth.tar \
+--gpu 3
 
 CUDA_VISIBLE_DEVICES=3 \
+python main_cedice_ours.py  \
+--exp_dir results/0306_bs8_DiceCE_normaltransform_nolradjust_simclrfinalpretrain \
+--batch_size 8 \
+--epochs 150 \
+--mode stride \
+--fold 1 \
+--save_freq 50 \
+--print_freq 100 \
+--temp 0.1 \
+--resume /home1/yichen/SemiContrast/results/1210_simclr_bs16/final_model.pth \
+--gpu 3
+
+
+
+
+CUDA_VISIBLE_DEVICES=0 \
+python main_cedice_ours.py  \
+--exp_dir results/0306_bs8_DiceCE_normaltransform_nolradjust_coseglocalfinalpretrain \
+--batch_size 8 \
+--epochs 150 \
+--mode stride \
+--fold 1 \
+--save_freq 50 \
+--print_freq 100 \
+--temp 0.1 \
+--resume /home1/yichen/SemiContrast/results/0119_bs1_stride_Con_1gpu_lr000001_wd1e4_simclrfinalpretrain_nonormalize/models/SupCon_mmwhs_adam_fold_1_lr_1e-05_decay_0.0001_bsz_1_temp_0.1_train_1.0_stride_stride_4/ckpt.pth \
+--gpu 0
+
+CUDA_VISIBLE_DEVICES=0 \
 python main_cedice_ours.py  \
 --exp_dir results/0306_bs8_DiceCE_normaltransform_nolradjust_cosegsemifinalpretrain \
 --batch_size 8 \
@@ -160,4 +185,4 @@ python main_cedice_ours.py  \
 --print_freq 100 \
 --temp 0.1 \
 --resume /home1/yichen/SemiContrast/results/0119_bs1_stride_SupconCon_1gpu_lr000001_wd1e4_simclrfinalpretrain_nonormalize/models/SupCon_mmwhs_adam_fold_1_lr_1e-05_decay_0.0001_bsz_1_temp_0.1_train_1.0_stride_stride_4/ckpt.pth \
---gpu 3
+--gpu 0
